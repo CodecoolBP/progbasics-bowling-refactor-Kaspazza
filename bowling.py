9e01af9b9e20d054a3a622f665391365a1438488
Mateusz Mazurczak <kaspazza@gmail.com>
2018-03-19 22:39:15 +0100

    Cleaner

--- bowling.py
+++ bowling.py
@@ -6,13 +6,13 @@ def score(game):
             result += 10 - last
         else:
             result += value(game[i])
-        if frame < 10 and game[i] in ('/', 'X', 'x') :
-                result += value(game[i + 1])
-                if game[i] in ('X', 'x'):
-                    if game[i + 2] == '/':
-                        result += 10 - value(game[i + 1])
-                    else:
-                        result += value(game[i + 2])
+        if frame < 10 and game[i] in ('/', 'X', 'x'):
+            result += value(game[i + 1])
+            if game[i] in ('X', 'x'):
+                if game[i + 2] == '/':
+                    result += 10 - value(game[i + 1])
+                else:
+                    result += value(game[i + 2])
         last = value(game[i])
         if first_half:
             first_half = False
